b9be23afc45ac13ef8da2051ebaa422934802335
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-03-26 21:55:30 -0700

    Updated the node version and worked on the Hero_content component

--- my-portfolio/app/page.tsx
+++ my-portfolio/app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/main/Hero";
 export default function Home() {
   return (
     <main className='h-full w-full'>
-      <div className='flex flex-col h-[360] gap-5'>
+      <div className='flex flex-col h-[850] gap-5'>
         <Hero />
 
       </div>
