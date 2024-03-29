24f63f1ad45e46de9934516182c79bc17c9986bc
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-03-28 21:36:38 -0700

    Added new CSS classes to the global css file and continued work on the hero section

--- my-portfolio/components/sub/Hero_Content.tsx
+++ my-portfolio/components/sub/Hero_Content.tsx
@@ -1,5 +1,7 @@
 import React from 'react'
 import { motion } from 'framer-motion'
+import { slideInFromTop } from '@/utility/motion'
+import { SparklesIcon } from '@heroicons/react/24/solid'
 
 
 const Hero_Content = () => {
@@ -7,6 +9,16 @@ const Hero_Content = () => {
     initial="hidden"
     animate="visible"
     className ='flex flex-row items-center justify-center px-20 mt-40 w-full z-[20]'>
+        <div className='h-full w-full flex flex-col gap-5 justify-center m-auto text-start'>
+            <motion.div
+            variants={slideInFromTop}
+            className='welcome-box py-15px border border-[#7042f88b] opacity-[0.9]'>
+                <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
+                <h1>Software Developer + QA Professional</h1>
+
+            </motion.div>
+
+        </div>
     
 
     </motion.div>
