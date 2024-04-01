0b441d1af07b6166a5e4afc095cc203e121379b4
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-03-31 18:04:27 -0700

    More Work on the hero section for the homepage

--- my-portfolio/components/sub/Hero_Content.tsx
+++ my-portfolio/components/sub/Hero_Content.tsx
@@ -1,7 +1,7 @@
 "use client";
 import React from 'react'
 import { motion } from 'framer-motion'
-import { slideInFromTop } from '@/utility/motion'
+import { slideInFromLeft, slideInFromTop } from '@/utility/motion'
 import { SparklesIcon } from '@heroicons/react/24/solid'
 
 
@@ -17,9 +17,21 @@ const Hero_Content = () => {
             variants={slideInFromTop}
             className='welcome-box py-15px border border-[#7042f88b] opacity-[0.9]'>
                 <SparklesIcon className='text-[#b49bff] mr-[10px] h-5 w-5' />
-                <h1>Software Developer + QA Professional</h1>
+                <h1>Portfolio</h1>
 
             </motion.div>
+
+
+
+            <motion.div
+            variants={slideInFromLeft(0.5)}
+            className='flex flex-col gap-6 mt-6 text-6x1 text-bold text-white max-w-[600px] w-auto h-auto'>
+                John K
+                <span className="text-transparent bg-clip-text bg-gradient-to-r from-purple-500 to-cyan-500">Software Engineer and</span>
+                Game QA Professional
+            </motion.div>
+
+
 
         </div>
     
