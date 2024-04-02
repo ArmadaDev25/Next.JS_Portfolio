d6506d3a45939d627130cf0c4ff5efd3a5263b5b
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-04-01 18:29:07 -0700

    began setting up the projects page

---
+++ my-portfolio/app/projects/page.tsx
@@ -0,0 +1,12 @@
+
+export default function Projects() {
+    return (
+        <div className='relative flex flex-col h-full w-full'>
+            <video autoPlay muted loop className='rotate-180 absolute top-[-340px]'>
+                <source src='/blackhole.webm' type='video/webm'/>
+
+            </video>
+        </div>
+
+    )
+}
