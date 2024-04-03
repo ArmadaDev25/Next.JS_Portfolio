db94f883a0177ce88a1cd0878062ae0b6c9a174d
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-04-02 20:46:31 -0700

    Continued work on the star background

--- my-portfolio/components/main/StarBackground.tsx
+++ my-portfolio/components/main/StarBackground.tsx
@@ -1,8 +1,15 @@
 "use client"
 import React, {useState, useRef, Suspense} from "react"
 import {Canvas, useFrame } from '@react-three/fiber'
+import {Points, PointMaterial, Preload} from '@react-three/drei'
+// @ts-ignore
+import * as random from 'maath/random/dist/maath-random.esm'
 
-export default function StarBackground () {
+export default function StarBackground (props: any) {
+        const ref: any = useRef()
+        const [sphere] = useState(() =>
+        random.inSphere(new Float32Array(5000), {radius: 1.2})
+        );
     return(
         <div></div>
 
