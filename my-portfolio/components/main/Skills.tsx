17e3940edca8ef912a194cae1d497a31c0506a71
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-04-06 22:47:26 -0700

    Added in PNGs and Added content to the data file

---
+++ my-portfolio/components/main/Skills.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Skills = () => {
+    return(
+        <section
+        id='skills'
+        className='flex flex-col items-center justify-center game-3 h-full relative overflow-hidden py-20'>
+
+        </section>
+    )
+
+}
+
+export default Skills
--- my-portfolio/public/Data.ts
+++ my-portfolio/public/Data.ts
@@ -3,5 +3,80 @@ export const Projects = [
 
 ]
 export const GameCredits = [
-    
+    {
+        game_name: 'Forza Motorsport',
+        role: 'QA Tester (Test Associate)',
+        release:'2023'
+    },
+    {
+        game_name: 'Fortnite',
+        role: 'QA Tester (Post Launch)',
+        release:'2017'
+    },
+    {
+        game_name: 'Age of Empires 4',
+        role: 'QA Tester (Test Associate)',
+        release:'2021'
+    },
+    {
+        game_name: 'Gears 5',
+        role: 'QA Tester (Test Associate - Post Launch)',
+        release:'2019'
+    },
+    {
+        game_name: 'Age of Empires 3: Defiinitive Edition',
+        role: 'QA Tester (Test Associate)',
+        release:'2020'
+    },
 ]
+
+export const skills_Icons = [
+    {
+      skill_name: "Html 5",
+      Image: "/html.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Css",
+      Image: "/css.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Java Script",
+      Image: "/js.png",
+      width: 65,
+      height: 65,
+    },
+    {
+      skill_name: "Tailwind Css",
+      Image: "/tailwind.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "React",
+      Image: "/react.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Type Script",
+      Image: "/ts.png",
+      width: 80,
+      height: 80,
+    },
+    {
+      skill_name: "Next js 13",
+      Image: "/next.png",
+      width: 80,
+      height: 80,
+    },
+    {
+        skill_name: "Python",
+        Image: "/python.png",
+        width: 80,
+        height: 80,
+    },
+  ];
