792fac689649de7fed0dc1d1ffb961e3d390d712
john kvassay <119649188+ArmadaDev25@users.noreply.github.com>
2024-04-09 20:38:11 -0700

    finished with the skillsIcons component

--- my-portfolio/components/sub/SkillsIcons.tsx
+++ my-portfolio/components/sub/SkillsIcons.tsx
@@ -1,6 +1,9 @@
+"use client"
+
 import React from "react"
 import {motion} from 'framer-motion'
 import { useInView } from "react-intersection-observer"
+import Image from "next/image";
 
 interface Props {
     src: string;
@@ -15,8 +18,29 @@ const SkillsIcons = ({src, width, height, index}: Props) => {
     })
 
     const imageVariants = {
-        
+        hidden: {opacity: 0},
+        visible: {opacity: 1}
     }
+    
+    const animationDelay = 0.3
+
+
+    return (
+        <motion.div
+        ref={ref}
+        initial="hidden"
+        variants={imageVariants}
+        animate={inView ? "visible" : "hidden"}
+        custom={index}
+        transition={{delay: index * animationDelay}}>
+            <Image
+            src={src}
+            width={width}
+            height={height}
+            alt='skill image'/>
+
+        </motion.div>
+    )
 
 }
 export default SkillsIcons
